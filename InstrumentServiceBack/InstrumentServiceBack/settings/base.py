00e015e047bbd7c0b145881c3443538270d3570d
AdekWhat <AdekWhat1996@gmail.com>
2020-05-08 03:36:11 +0300

    back + front

---
+++ InstrumentServiceBack/InstrumentServiceBack/settings/base.py
@@ -0,0 +1,80 @@
+import environ
+
+project_root = environ.Path(__file__) - 4
+print(project_root)
+env = environ.Env(DEBUG=(bool, False),)
+CURRENT_ENV = 'dev' # 'dev' is the default environment
+
+# read the .env file associated with the settings that're loaded
+env.read_env('./InstrumentServiceBack/{}.env'.format(CURRENT_ENV))
+
+DATABASES = {
+    'default': env.db()
+}
+
+
+
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG')
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # 3rd party
+    'rest_framework',
+    # Local
+    'index',
+
+]
+
+
+ROOT_URLCONF = 'InstrumentServiceBack.urls'
+
+
+STATIC_URL = '/static/'
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+
+STATICFILES_DIRS = [
+    env('FRONTEND_ROOT')
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [env('FRONTEND_ROOT')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+REST_FRAMEWORK = {
+'DEFAULT_PERMISSION_CLASSES': [
+'rest_framework.permissions.AllowAny',
+]
+}
---
+++ InstrumentServiceBack/InstrumentServiceBack/settings/dev.py
@@ -0,0 +1,4 @@
+from InstrumentServiceBack.settings.base import *
+
+
+CURRENT_ENV = 'dev'
